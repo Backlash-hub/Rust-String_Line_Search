50c38aef23cb1427fe5150e60c014c13f18f513b
Backlash <williampevytoe@gmail.com>
2026-02-10 21:56:21 -0500

    return result

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //Insert a string and a path to txt document. Will return lines with given string
 // cargo run -- quick -- .\src\fox.txt will return "The quick"
+use anyhow::{Context, Result};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -8,13 +9,16 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("failed to read file");
     
+    let content = std::fs::read_to_string(&args.path)
+    .with_context(|| format!("Failed to read file {}", args.path.display()))?;
+
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    Ok(())
 }
